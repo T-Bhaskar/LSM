b92554882f0305aa37358f3d5aac69dbbd12120d
bhaskar <bhaskartiwari258@gmail.com>
2021-11-06 17:07:14 +0530

    updated latest info

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
-
-<?php include "lib_inc/header.php" ; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>School Management Project</title>
+    <style>
+        
+    </style>
+</head>
+<body>
+<div class="fixheaderm"><?php include "lib_inc/header.php" ; ?></div>
+<div class = "content">
 <?php include "lib_inc/slider.php"; ?>
 <?php include "lib_inc/latestinfo.php" ?>
 <?php include "lib_inc/aboutschool.php" ; ?>
@@ -11,4 +23,8 @@
 
 
 <?php include "lib_inc/footer.php" ; ?>
+    </div>
+</body>
+</html>
+
 
--- lib_inc/latestinfo.php
+++ lib_inc/latestinfo.php
@@ -7,30 +7,72 @@
     <style>
         .latecontainer{
             position: relative;
+            margin-top:15px;
+            margin-left: 10%;
+            margin-right: 10%;
+            border: 1px solid;
+  padding: 10px;
+  box-shadow: 5px 10px blue;
 
         }
         .latecontainer .latehead{
-            height:70px;
+            height:40px;
             wudth: 100%;
             font-size: 20px;
             background-color:blue;
+            color:#fff;
+            padding: 4px;
 
             
         }
+        .latecontainer .latehead center h3{
+           
+            margin-bottom: auto;
+
+        }
         .latecontainer .latebody {
             width: 100%;
-            height: max-content;
-            background-color:yellow;
-            color:black;
+            height: 300px;
+            background-color:red;
+            color:#fff;
+            padding: 10px;
+            overflow:auto;
+            margin-top:5px;
+            
 
+        }
+        .latecontainer .latebody latebodycontent{
+            margin-left 10px;
         }
     </style>
 </head>
 <body>
   <div class="latecontainer">
-      <div class="latehead">Latest Info</div>
+      <div class="latehead"><center><h3>Latest Info</h3></center></div>
+      
       <div class="latebody">
-          jdcjsdfhjashdjashdjashdj
+          <li class = "latebodycontent"> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores assumenda aut inventore cumque. Eius, officiis!</li>
+          <li class = "latebodycontent">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit, similique.</li>
+          <li class = "latebodycontent">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, ipsam?</li>
+          <li class = "latebodycontent">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, voluptatum.</li>
+          <li class = "latebodycontent">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, autem?</li>
+          <li class = "latebodycontent"> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores assumenda aut inventore cumque. Eius, officiis!</li>
+          <li class = "latebodycontent">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit, similique.</li>
+          <li class = "latebodycontent">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, ipsam?</li>
+          <li class = "latebodycontent">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, voluptatum.</li>
+          <li class = "latebodycontent"> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores assumenda aut inventore cumque. Eius, officiis!</li>
+          <li class = "latebodycontent">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit, similique.</li>
+          <li class = "latebodycontent">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, ipsam?</li>
+          <li class = "latebodycontent">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, voluptatum.</li>
+          <li class = "latebodycontent"> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores assumenda aut inventore cumque. Eius, officiis!</li>
+          <li class = "latebodycontent">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit, similique.</li>
+          <li class = "latebodycontent">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, ipsam?</li>
+          <li class = "latebodycontent">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, voluptatum.</li>
+          <li class = "latebodycontent"> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores assumenda aut inventore cumque. Eius, officiis!</li>
+          <li class = "latebodycontent">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit, similique.</li>
+          <li class = "latebodycontent">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, ipsam?</li>
+          <li class = "latebodycontent">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, voluptatum.</li>
+        
       </div>
 
   </div>  
